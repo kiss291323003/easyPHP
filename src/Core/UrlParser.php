35db87196f2ed7c7e97203014c929cd0bce86a0c
kiss291332003 <z15935700>
2017-06-13 17:43:56 +0800

    optimize urlParser

--- src/Core/UrlParser.php
+++ src/Core/UrlParser.php
@@ -18,22 +18,16 @@
             $httpRequest = Request::getInstance();
             //优先检测pathinfo模式  否则用uri路径覆盖
             $pathInfo = $httpRequest->getServer("REDIRECT_URL") ? $httpRequest->getServer("REDIRECT_URL") : '/';
-            //反编码
-            $pathInfo = rawurldecode($pathInfo);
-            //去除尾缀   如.html .do
-            $post = strripos($pathInfo,'.');
-            if($post !== false){
-                $pathInfo = substr($pathInfo,0,$post);
+            $basePath = dirname($pathInfo);
+            $info = pathInfo($pathInfo);
+            if($info['filename'] != 'index'){
+                if($basePath == '/'){
+                    $basePath = $basePath.$info['filename'];
+                }else{
+                    $basePath = $basePath.'/'.$info['filename'];
+                }
             }
-            //去除右边 index
-            if(substr($pathInfo,-6) == '/index'){
-                $pathInfo = substr($pathInfo,0,-5);
-            }
-            //去除右边 /    在空路径时   请勿去除
-            if(strlen($pathInfo) > 1){
-                $pathInfo = rtrim($pathInfo,"/");
-            }
-            return $pathInfo;
+            return $basePath;
         }
 
 	}
